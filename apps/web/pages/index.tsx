fdaebce3ea0ffbd932871357db7b0940a4c61c5d
Riley1101 <www.riley.s.eileen@gmail.com>
2022-05-05 22:09:11 +0630

    update for demo app

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,35 @@
-import { Button, Input, Typography } from 'storybookui'
+import { Button, Input, Typography } from 'storybookui';
+import styles from './index.module.css';
+
 export default function Web() {
   return (
-    <div>
-      <Typography role='h1' >Hello World</Typography>
-      <Input label='Usernmae' name='username' />
-      <Button label='Hello World' variant='secondary'></Button>
+    <div className={styles.container}>
+      <Typography role='h1' size="5xl" sx={{ marginBlock: '.5em' }}> Design System Demo</Typography>
+      <Typography role='p'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Provident architecto maiores nesciunt, amet velit quae aliquid,
+        illum aperiam dolorum quos sed neque eius consectetur, mollitia nam nulla
+        enim minus libero debitis minima corporis! Ab magnam quaerat dolorem aliquid cum,
+        odio tempore vitae praesentium facere quam, nobis commodi tenetur natus esse eveniet
+        expedita ullam veritatis ratione libero voluptatum quod consectetur reprehenderit totam
+        harum! Sapiente nobis eveniet debitis dolorum temporibus tenetur non ea,
+        ipsa dolore repudiandae et quisquam. Unde fugiat voluptatum voluptatibus dignissimos soluta
+        doloremque in corporis alias quam quod perferendis excepturi vitae nulla ullam,
+        aspernatur tempore! Iusto natus maxime inventore repellat.
+      </Typography>
+      <Typography role='a' sx={{ marginBlock: "1em", display: 'block' }}>
+        Link
+      </Typography>
+      <form action="" className={styles.form}>
+        <Input label='Usernmae' name='username' placeholder="Username" />
+        <Input label='Password' name='Password' placeholder='Password' />
+        <div className={styles.hstack}>
+          <Button label='Cancel' variant='secondary'></Button>
+          <Button label='Submit' variant='primary'></Button>
+
+        </div>
+
+      </form>
     </div>
   );
 }
